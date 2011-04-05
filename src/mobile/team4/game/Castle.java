83cb9486269c3426b0a1bc9cb7db7215f3b420e0
Shanief Webb <shaniefwebb@Shanief-Webbs-MacBook.local>
2011-04-05 11:04:52 -0400

    added some code

--- src/mobile/team4/game/Castle.java
+++ src/mobile/team4/game/Castle.java
@@ -1,5 +1,27 @@
 package mobile.team4.game;
 
-public class Castle extends GameObject {
+import java.util.ArrayList;
 
+public class Castle extends GameObject {
+	ArrayList<WallPiece> wallPieces;
+	
+	Castle(int x, int y)
+	{
+		super.setPosition(x, y);
+	}
+	
+	boolean addWallPiece(WallPiece wp)
+	{
+		if (GameMap.get_at(wp.getPosition().get_x(), wp.getPosition().get_y()) == 0)
+		{
+			wp.setPosition(-1,-1);	
+			return (false);
+		}
+		return true;
+	}
+	
+	public boolean isFullyEnclosed()
+	{
+	  return (false);
+	}
 }
